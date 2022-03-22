bb292b829648ddadea371dd97c8143fe6ceca73a
Kasra Esfahanian <kasraesf1383@gmail.com>
2022-03-22 15:35:07 -0700

    added privacy policy and terms of use to the app

--- Main/scripts/authentication.js
+++ Main/scripts/authentication.js
@@ -26,15 +26,14 @@ var uiConfig = {
       }
       return false;
     },
-    uiShown: function () {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
-    }
+    // uiShown: function () {
+    //   // The widget is rendered.
+    //   // Hide the loader.
+    //   document.getElementById('loader').style.display = 'none';
+    // }
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: './index.html',
+  signInSuccessUrl: 'index.html',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -45,9 +44,9 @@ var uiConfig = {
     //firebase.auth.PhoneAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
-  tosUrl: '<your-tos-url>',
+  tosUrl: 'https://www.termsofusegenerator.net/live.php?token=ybFLQ9ktfqT1tjdKyMG4NwRSIsY78oVY',
   // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
+  privacyPolicyUrl: 'https://www.termsfeed.com/live/bec3103c-4a7f-4b27-a797-964d4478a590'
 
 };
 
